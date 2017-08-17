3f11adac0985e3ec18aee9758fb57e840e7d43ca
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-08-17 19:07:27 +0200

    rearranged the different copies into one runable comparison, not much more is done but it at least works

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+# calling jupyter with this option avoids an error message regarding bandwidth rescrictions leading to errors execution the notebooks for the PNAS paper
 jupyter notebook --NotebookApp.iopub_data_rate_limit=10000000 
