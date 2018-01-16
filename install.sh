265b5bfb71062eb71f7c69f57190cef3f1eeb763
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-01-16 19:25:04 +0100

    tox is a tool that can check our package against different python versions like python3.5 python3.6 and so on without the necessity to install all of them on our buildslave or in our home directory

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ pip3 install -e -r requirements.txt .
 #python3 setup.py develop
 
 # enable jupyter notebook nbextensions
+pip3 install tox #testtool to chech the lib against different python versions
 pip3 install concurrencytest 
 pip3 install --upgrade jupyter jupyter_contrib_nbextensions jupyter_nbextensions_configurator
 
