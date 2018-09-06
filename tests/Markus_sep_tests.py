429fd5574c7b3efc19112e1977dfbf1ed0b3ba99
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-09-06 22:04:30 +0200

    isoloated the failing tox test

--- tests/Markus_sep_tests.py
+++ tests/Markus_sep_tests.py
@@ -7,11 +7,11 @@ from Test_smooth_model_run import TestSmoothModelRun
 from Test_helpers_reservoir import TestHelpers_reservoir
 from Test_start_distributions import TestStartDistributions
 s=unittest.TestSuite()
-s.addTest(TestSmoothReservoirModel('test_jacobian'))
-s.addTest(TestSmoothModelRun("test_age_densities"))
-s.addTest(TestStartDistributions("test_numeric_staedy_state"))
-s.addTest(TestStartDistributions("test_compute_start_age_moments_from_steady_state"))
-s.addTest(TestStartDistributions("test_start_age_distribuions_from_steady_state"))
+#s.addTest(TestSmoothReservoirModel('test_jacobian'))
+#s.addTest(TestSmoothModelRun("test_age_densities"))
+#s.addTest(TestStartDistributions("test_numeric_staedy_state"))
+#s.addTest(TestStartDistributions("test_compute_start_age_moments_from_steady_state"))
+#s.addTest(TestStartDistributions("test_start_age_distribuions_from_steady_state"))
 s.addTest(TestStartDistributions("test_start_age_distribuions_from_empty_spinup"))
-s.addTest(TestStartDistributions("test_start_age_moments_from_empty_spinup"))
+#s.addTest(TestStartDistributions("test_start_age_moments_from_empty_spinup"))
 unittest.TextTestRunner(verbosity=2).run(s)
