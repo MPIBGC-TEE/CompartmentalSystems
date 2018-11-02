1a06c7542d7125621e88323505393b89ed97325a
mm <markus.mueller.1.g@googlemail.com>
2018-11-02 01:01:55 +0100

    added deceleration limiter

--- prototypes/limiters.py
+++ prototypes/limiters.py
@@ -2,6 +2,7 @@ from sympy import lambdify
 from sympy import lambdify, symbols
 from sympy import solve
 import numpy as np
+import matplotlib.pyplot as plt
 y,y_max,z,z0,alpha =symbols('y,y_max,z,z0,alpha')
 a0,a1,a2,a3=symbols('a0,a1,a2,a3')
 f=a3*z**3+a2*z**2+a1*z+a0
@@ -18,4 +19,26 @@ par_dict=solve([
 f_ai=f.subs(par_dict)
 f_par=f_ai.subs({z0:50})
 f_num=lambdify(z,f_par,modules='numpy')
-print(f_num(np.linspace(1,50,100)))
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+z_val=np.linspace(1,50,100)
+ax.plot(z_val,f_num(z_val))
+
+#decelaration
+f=1/z0*(1-(1-z)**alpha)
+f
+f.diff(z)
+f.diff(z).subs(z,0)
+f=1/z0*(1-(z0-z)**alpha)
+f.diff(z).subs(z,0)
+f.diff(z).subs(z,0)-1
+solve(f.diff(z).subs(z,0)-1,z)
+solve(f.diff(z).subs(z,0)-1,alpha)
+solve(f.diff(z).subs(z,0)-1,alpha)[0]
+alpha=solve(f.diff(z).subs(z,0)-1,alpha)[0]
+alpha.subs(z0,50)
+alpha_val=alpha.subs(z0,50)
+f.subs({z0:50})
+f.subs({z0:50,alpha:alpha_val})
+alpha_val
+f.subs({z0:50,alpha:alpha_val})
