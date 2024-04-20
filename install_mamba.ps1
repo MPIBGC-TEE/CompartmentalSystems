8d7e4b79aa6be353712a1b84e3b214660d00af9a
Markus Müller <markus.mueller.1.g@googlemail.com>
2024-04-20 15:38:58 +0200

    removed the call since this is not powershell syntax

--- install_mamba.ps1
+++ install_mamba.ps1
@@ -24,27 +24,27 @@
 # detected by pip. 
 # This happens only for some packages (e.g.  # python-igraph) 
 # but is a known problem at the time of writing and does affect us.
-call  conda install -y -c conda-forge matplotlib mpmath numpy plotly scipy sympy tqdm frozendict networkx pandas python-igraph pycairo pip
+conda install -y -c conda-forge matplotlib mpmath numpy plotly scipy sympy tqdm frozendict networkx pandas python-igraph pycairo pip
 
 
 # To run the tests (which is recommended but not necessary for the package to work) 
 # you need some extra packages which can be installed by uncommenting the following line:
-call  conda install -y -c conda-forge dask[distributed] jupyterlab jupytext nbformat nbmake notebook pytest zarr
+conda install -y -c conda-forge dask[distributed] jupyterlab jupytext nbformat nbmake notebook pytest zarr
 
 
 # If you want to develop the package including the documentation you need some extra tools which can be installed by uncommenting the following line:
-# call  conda install -y -c conda-forge sphinx sphinx-autodoc-typehints
+# conda install -y -c conda-forge sphinx sphinx-autodoc-typehints
 
 
 # We install the dependencies that are not on pypy directly from github repos
 # This is not possible with conda (therefore we use pip here)
 # Do not do this (comment the following lines) 
 # if you have checked out these repos and installed the code in developer mode
-call  pip install --no-deps git+https://github.com/MPIBGC-TEE/testinfrastructure.git#egg=testinfrastructure git+https://github.com/MPIBGC-TEE/LAPM.git#egg=LAPM
+pip install --no-deps git+https://github.com/MPIBGC-TEE/testinfrastructure.git#egg=testinfrastructure git+https://github.com/MPIBGC-TEE/LAPM.git#egg=LAPM
 
 # The following line installs the package without checking out the repository directly from github
-call  pip install --no-deps git+https://github.com/MPIBGC-TEE/CompartmentalSystems.git@test#egg=CompartmentalSystems
+pip install --no-deps git+https://github.com/MPIBGC-TEE/CompartmentalSystems.git@test#egg=CompartmentalSystems
 
 # if you want to install it in develop mode (after checking out the repo) comment the previous line and
 # execute the following line instead, in the same directory where this file lives. 
-# call  pip install --no-deps -e .
+# pip install --no-deps -e .
